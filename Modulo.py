54092e50cd5b87d995895275d7979e1bdfb692aa
[Jeff.Knecht] <[jeffdknecht@gmail.com]>
2025-02-21 15:44:13 -0800

    first program

---
+++ Modulo.py
@@ -0,0 +1,8 @@
+prompt=input("Please give me a number, I will tell you if its divisible by 10")
+
+digit=int(prompt)
+
+if digit % 10 == 0:
+        print("This is divisible by 10!.")
+else:
+        print("Please please try again.")
